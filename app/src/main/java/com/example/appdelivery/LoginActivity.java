b7cae5e3ef21068c9f1182f24e5a3a1758358d6b
unknown <GUSTAVO_4943@UIRAPURU.BR>
2026-04-28 16:13:25 -0300

    app feito ate o slide 60 com dificuldade

--- app/src/main/java/com/example/appdelivery/LoginActivity.java
+++ app/src/main/java/com/example/appdelivery/LoginActivity.java
@@ -7,16 +7,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
 
+    TextView criarUsuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_acticity);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
